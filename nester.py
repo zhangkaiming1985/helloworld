8447c799393a0af3cff60215e973b86a28dddf0e
zhangkaiming1985 <34681444+zhangkaiming1985@users.noreply.github.com>
2017-12-27 22:06:28 +0800

    pc 2017-12-27 22：06

--- nester.py
+++ nester.py
@@ -2,6 +2,7 @@
 
 这是nester.py模块，提供了一个名为print_lol的函数用来打印列表，
 其中包含或不包含嵌套列表。
+增加一个名为print_file的函数，用来将内容输入到文件中，并保持print_lol的特性。
 
 """
 
@@ -20,3 +21,26 @@ def print_lol(the_list, level=0):
             for tab_step in range(level):
                 print('\t', end='')
             print(each_item)
+
+
+def print_file(the_list, the_file, indent = True, the_separator = ''):
+    """
+
+    :param the_list:位置参数，所提供的列表将递归输入到文件中，各占一行。
+    :param the_file:写入文件名。
+    :param indent:是否分割，True代表默认分割。
+    :param the_separator:分隔符，默认为空。
+    :return: NONE
+
+    """
+    try:
+        with open(the_file, 'w') as date:
+            for each_item in the_list:
+                try:
+                    print(each_item, file=date, end='')
+                    if indent:
+                        print(the_separator, file=date)
+                except ValueError as err:
+                    print("value error:" + str(err))
+    except IOError as err:
+        print("file error:" + str(err))
